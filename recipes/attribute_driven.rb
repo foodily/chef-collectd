2f3b74c3262f6b4c087e1baeaa9fbefef0f31c6f
jeffkimble <foodily@jeffkimble.com>
2014-07-21 14:39:06 -0700

    debugging

--- recipes/attribute_driven.rb
+++ recipes/attribute_driven.rb
@@ -8,7 +8,7 @@ if node["collectd"]["plugins"].key?("write_graphite")
 
 Chef::Log.debug ("Graphite Search: ||"+ node.collectd.graphite_search + "||")
 
-    graphite_server_results = search(:node, node["collectd"]["graphite_search"])
+    graphite_server_results = search(:node, " #{node["collectd"]["graphite_search"]}" )
 
     if graphite_server_results.empty?
       Chef::Application.fatal!("Graphite plugin enabled but no Graphite server found.")
